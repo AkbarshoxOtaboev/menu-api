88331b3001e87fae67247ec8b906f13471426822
Admin <otaboevakbar96@gmail.com>
2024-10-30 16:48:43 +0500

    Spring security 30%

--- src/main/java/uz/script/menu/services/UserService.java
+++ src/main/java/uz/script/menu/services/UserService.java
@@ -1,5 +1,12 @@
 package uz.script.menu.services;
 
-public interface UserService {
+import org.springframework.security.core.userdetails.UserDetailsService;
+import uz.script.menu.entities.User;
 
+public interface UserService {
+    User  save(User user);
+    User create(User user);
+    User getByUsername(String username);
+    UserDetailsService userDetailsService();
+    User getCurrentUser();
 }
